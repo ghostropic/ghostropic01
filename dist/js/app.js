5c1d7bf833f43dc7f863bdf1c48e24cddc838b99
Jon Bichar <jon.bichar@gmail.com>
2016-01-22 12:57:50 -0500

    fcommit

---
+++ dist/js/app.js
@@ -0,0 +1,2 @@
+$(".nav-toggle").click(function(){$(this).toggleClass("active"),$(".header-nav").toggleClass("open"),event.preventDefault()}),$(".header-nav li a").click(function(){$(".nav-toggle").toggleClass("active"),$(".header-nav").toggleClass("open")});
+//# sourceMappingURL=../maps/app.js.map
---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+/***************** Nav Transformicon ******************/
+
+
+	/* When user clicks the Icon */
+	$('.nav-toggle').click(function() {
+		$(this).toggleClass('active');
+		$('.header-nav').toggleClass('open');
+		event.preventDefault();
+	});
+	/* When user clicks a link */
+	$('.header-nav li a').click(function() {
+		$('.nav-toggle').toggleClass('active');
+		$('.header-nav').toggleClass('open');
+
+	});
+    
+
+
+
+
